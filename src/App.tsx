2497826ecf420082311f7919a3f5232309dd8b95
Brandon Thigpen <brandonkthigpen@gmail.com>
2023-03-31 09:09:48 -0400

    Fixes typo

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const predefinedDateRanges = [
 function App(): JSX.Element {
   return (
     <div className="h-screen bg-slate-50">
-      <div className="container= mx-auto px-4">
+      <div className="container mx-auto px-4">
         <Calendar dates={predefinedDateRanges} />
       </div>
     </div>
